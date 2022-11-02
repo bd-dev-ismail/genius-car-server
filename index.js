32c7ffebb636c6d106afd2bc4da918788e2baadd
bd-dev-ismail <ismailratul7@gmail.com>
2022-11-02 07:25:08 +0600

    query by email & get orders data

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -17,11 +17,54 @@ const client = new MongoClient(uri, {
   useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1,
 });
-client.connect((err) => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
+
+async function run(){
+
+    try{
+        const servicesCollection = client.db('geinusCar').collection('services');
+        const ordersCollection = client.db('geinusCar').collection('orders');
+        //get services 
+        app.get('/services', async(req, res)=>{
+            const query = {};
+            const cursor = servicesCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services)
+        });
+        //get single services
+        app.get('/services/:id', async(req, res)=>{
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)}
+            const services = await servicesCollection.findOne(query);
+            res.send(services);
+        });
+        //order apis
+        app.get('/orders', async(req, res)=>{
+            // console.log(req.query.email);
+            let query = {};
+
+            if(req.query.email){
+                query = {
+                    email: req.query.email,
+                }
+            }
+
+            const cursor = ordersCollection.find(query);
+            const orders = await cursor.toArray();
+            res.send(orders);
+        })
+
+        app.post('/orders', async(req, res)=>{
+            const order = req.body;
+            const result = await ordersCollection.insertOne(order);
+            res.send(result)
+        })
+    }
+    finally{
+
+    }
+}
+
+run().catch(err => console.error(err))
 
 
 
